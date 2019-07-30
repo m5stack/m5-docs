eda8092e98ffa38f5f304f8a581481ce0803f268
shaoxiang <1026025056@qq.com>
2019-07-30 13:47:30 +0800

    change css file debug test

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -22,11 +22,11 @@
      *   <link rel="stylesheet" title="Bar" href="path/to/file.css" >
      */
     function initStyleSwitcher() {
-        var isInitialzed      = false;
+        var isInitialzed = false;
         var sessionStorageKey = 'activeStylesheetHref';
 
         function handleSwitch(activeHref, activeTitle) {
-            var activeElm = document.querySelector('link[href*="' + activeHref +'"],link[title="' + activeTitle +'"]');
+            var activeElm = document.querySelector('link[href*="' + activeHref + '"],link[title="' + activeTitle + '"]');
 
             if (!activeElm && activeHref) {
                 activeElm = document.createElement('link');
@@ -40,16 +40,15 @@
                     activeElm.removeEventListener('load', linkOnLoad);
                     setActiveLink(activeElm);
                 });
-            }
-            else if (activeElm) {
+            } else if (activeElm) {
                 setActiveLink(activeElm);
             }
         }
 
         function setActiveLink(activeElm) {
-            var activeHref   = activeElm.getAttribute('href');
-            var activeTitle  = activeElm.getAttribute('title');
-            var inactiveElms = document.querySelectorAll('link[title]:not([href*="' + activeHref +'"]):not([title="' + activeTitle +'"])');
+            var activeHref = activeElm.getAttribute('href');
+            var activeTitle = activeElm.getAttribute('title');
+            var inactiveElms = document.querySelectorAll('link[title]:not([href*="' + activeHref + '"]):not([title="' + activeTitle + '"])');
 
             // Remove "alternate" keyword
             activeElm.setAttribute('rel', (activeElm.rel || '').replace(/\s*alternate/g, '').trim());
@@ -72,7 +71,7 @@
                 // required to properly apply style updates when alternate
                 // stylesheets are enabled.
                 if (window.browsersyncObserver) {
-                    var linkRel    = elm.getAttribute('rel') || '';
+                    var linkRel = elm.getAttribute('rel') || '';
                     var linkRelAlt = linkRel.indexOf('alternate') > -1 ? linkRel : (linkRel + ' alternate').trim();
 
                     elm.setAttribute('rel', linkRelAlt);
@@ -100,13 +99,13 @@
 
             // Update active stylesheet
             document.addEventListener('click', function(evt) {
-                var dataHref  = evt.target.getAttribute('data-link-href');
+                var dataHref = evt.target.getAttribute('data-link-href');
                 var dataTitle = evt.target.getAttribute('data-link-title')
 
                 if (dataHref || dataTitle) {
-                    dataTitle = dataTitle
-                        || evt.target.textContent
-                        || '_' + Math.random().toString(36).substr(2, 9); // UID
+                    dataTitle = dataTitle ||
+                        evt.target.textContent ||
+                        '_' + Math.random().toString(36).substr(2, 9); // UID
 
                     handleSwitch(dataHref, dataTitle);
                     evt.preventDefault();
